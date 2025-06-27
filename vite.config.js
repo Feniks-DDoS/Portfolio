a02f2947d3333979f04054e794485164db98b0b7
Valetyn <valentindikhtyarenko2004@gmail.com>
2025-06-28 00:52:12 +0900

    Fix src

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/Portfolio/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        privacy: resolve(__dirname, 'privacyPolice.html'),
+        terms: resolve(__dirname, 'terms.html'),
+        singleBlog: resolve(__dirname, 'single-blog.html'),
+        singleBlog2: resolve(__dirname, 'single-blog-2.html'),
+      }
+    }
+  }
 });
